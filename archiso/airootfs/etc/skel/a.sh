28adfa6281125c8ea25f61dcb6b92c435aa4288a
Santiago Calligari <89709491+SantiagoCalligari@users.noreply.github.com>
2022-12-31 17:54:00 -0300

    agrregle el script de instalacion

--- archiso/airootfs/etc/skel/a.sh
+++ archiso/airootfs/etc/skel/a.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-sudo rm -rf /etc/calamares/branding
+sudo rm -rf /etc/calamares/branding/default
 git clone https://github.com/SantiagoCalligari/aa
 cd aa
-sudo mv branding /etc/calamares/.
+sudo mv branding/default /etc/calamares/branding/default
 sudo rm -rf /etc/skel/a.sh /etc/skel/.config/autostart
 sudo rm -rf ~/a.sh ~/.config/autostart
 
